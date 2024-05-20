1e08f3c63a9f19401e67b5343639b8063d86cba8
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-19 23:03:57 -0400

    imgs new

--- src/blocks/block2.tsx
+++ src/blocks/block2.tsx
@@ -5,6 +5,8 @@ import img3 from "../assets/block2/sqr2.webp";
 import img4 from "../assets/block2/sqr3.webp";
 import img5 from "../assets/block2/sqr4.webp";
 import img6 from "../assets/block2/sqr5.webp";
+import img7 from "../assets/block2/sqr6.webp";
+import img8 from "../assets/block2/sqr7.webp";
 import { hoverScale } from "./block3";
 
 const imgClass =
@@ -14,7 +16,7 @@ export const Block2 = () => {
   return (
     <>
       <BlockWrapper>
-        <div className="grid lg:flex-row grid-cols-1 xl:grid-cols-3 md:grid-cols-2 gap-8 lg:gap-16 justify-center items-center w-full">
+        <div className="grid lg:flex-row grid-cols-1 xl:grid-cols-4 md:grid-cols-2 gap-8 lg:gap-16 justify-center items-center w-full">
           <img className={imgClass} src={img1} />
 
           <img className={imgClass} src={img2} />
@@ -26,6 +28,10 @@ export const Block2 = () => {
           <img className={imgClass} src={img5} />
 
           <img className={imgClass} src={img6} />
+
+          <img className={imgClass} src={img7} />
+
+          <img className={imgClass} src={img8} />
         </div>
       </BlockWrapper>
     </>
