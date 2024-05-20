eb13d8faa728a2ffe89fecfa6b7d312f85310ec8
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2024-05-19 21:56:32 -0400

    Block 1 improve

--- src/blocks/block1.tsx
+++ src/blocks/block1.tsx
@@ -6,25 +6,23 @@ export const Block1 = () => {
   return (
     <>
       <BlockWrapper>
-        <div className="flex flex-col justify-between items-center">
-          <Links customClass="mt-2" />
-          <div className="flex flex-row gap-16 items-center justify-between text-4xl md:text-6xl font-bold text-center">
-            <h3>WAF</h3>
-            <img
-              src={WAF}
-              alt="logo"
-              className="h-16 w-auto md:h-[60vh] rounded-2xl"
-            />
-            <h3>WAF</h3>
-          </div>
-          <div>
-            <h2 className="text-2xl md:text-4xl font-bold text-center">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
-              accusamus illo ab quia aspernatur, reiciendis nulla possimus odit
-              porro incidunt quisquam soluta voluptas. Obcaecati veniam nisi
-              accusantium earum ad sapiente.
-            </h2>
-          </div>
+        <Links customClass="" />
+        <div className="flex flex-col xl:flex-row gap-2 xl:gap-16 items-center justify-between text-4xl md:text-6xl font-bold text-center">
+          <h3>WAF</h3>
+          <img
+            src={WAF}
+            alt="logo"
+            className="w-full h-auto xl:w-auto xl:h-[60vh] rounded-2xl flipping-image"
+          />
+          <h3>WAF</h3>
+        </div>
+        <div>
+          <h2 className="text-xl xl:text-4xl font-bold text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit
+            accusamus illo ab quia aspernatur, reiciendis nulla possimus odit
+            porro incidunt quisquam soluta voluptas. Obcaecati veniam nisi
+            accusantium earum ad sapiente.
+          </h2>
         </div>
       </BlockWrapper>
     </>
