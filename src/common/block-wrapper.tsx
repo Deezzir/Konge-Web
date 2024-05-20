fdb2c2e47dbd28f15e882d3303540356be52789e
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-19 23:22:18 -0400

    animate on appear

--- src/common/block-wrapper.tsx
+++ src/common/block-wrapper.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 interface BlockWrapperProps {
   children: React.ReactNode;
@@ -6,8 +7,16 @@ interface BlockWrapperProps {
 
 export const BlockWrapper = (props: BlockWrapperProps) => {
   return (
-    <div className="relative min-h-screen flex-col gap-8 lg:gap-16 w-full flex p-4 md:p-16 justify-center max-w-screen-2xl items-center">
+    <motion.div
+      className="relative min-h-screen flex-col gap-8 lg:gap-16 w-full flex p-4 md:p-16 justify-center max-w-screen-2xl items-center"
+      initial={{ opacity: 0 }}
+      transition={{
+        duration: 2,
+      }}
+      whileInView={{ opacity: 1 }}
+      viewport={{ once: true }}
+    >
       {props.children}
-    </div>
+    </motion.div>
   );
 };
