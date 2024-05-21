8e8b9a0b885afaec779db132d51c1130061fa6ae
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-20 22:27:18 -0400

    1

--- src/common/urls.ts
+++ src/common/urls.ts
@@ -2,6 +2,9 @@ export const CA = "Coming";
 export const TELEGRAM = "https://t.me/WillemDaChoice";
 export const TWITTER = "https://x.com/WillemDaChoice";
 
-export const DEXSCREENER = "";
-export const RAYDIUM = "";
-export const JUPITER = "";
+export const DEXSCREENER =
+  "https://dexscreener.com/solana/9nbnDgUAQ6SzS9T8JvUEGCzAJGGqcx8k72HVFB5rMHjc";
+export const RAYDIUM =
+  "https://raydium.io/swap/?inputMint=sol&outputMint=4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R";
+export const JUPITER =
+  "https://jupiter-terminal.dexscreener.com/?inputMint=CeocaPX6WNeYKHvNKgKQC7wkMNNMViKgoTf2f87n1z7o&outputMint=So11111111111111111111111111111111111111112";
