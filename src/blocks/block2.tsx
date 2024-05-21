16e6aee75a088b82a3387dd8f52a6003249e5b31
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-20 20:37:10 -0400

    updated images

--- src/blocks/block2.tsx
+++ src/blocks/block2.tsx
@@ -1,24 +1,46 @@
 import { motion } from "framer-motion";
+import { useCallback } from "react";
 
 import Marquee from "react-fast-marquee";
 import { Img } from "react-image";
 
 export const hoverScale = "hover:scale-[0.99] transform transition duration-y";
 
+function createArrayFromNtoM(n: number, m: number) {
+  let array = [];
+  for (let i = n; i <= m; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
 export const Block2 = () => {
+  const arr1 = useCallback(() => {
+    return createArrayFromNtoM(1, 13);
+  }, []);
+  const arr2 = useCallback(() => {
+    return createArrayFromNtoM(14, 27);
+  }, []);
+  const arr3 = useCallback(() => {
+    return createArrayFromNtoM(28, 41);
+  }, []);
+  const arr4 = useCallback(() => {
+    return createArrayFromNtoM(42, 53);
+  }, []);
+
   const getImages = () => {
     return (
       <>
         <Marquee speed={15} autoFill direction={"left"}>
           <div className="flex flex-row gap-8 h-[25vh] ms-8">
-            {[...Array(7)].map((_, index: any) => {
+            {arr1().map((val: number, index: any) => {
               return (
                 <Img
                   className={
                     "w-auto h-auto rounded-2xl select-none object-contain " +
                     hoverScale
                   }
-                  src={"../assets/block2/sqr0.webp"}
+                  src={`./block2/will${val}.webp`}
                   key={index}
                 />
               );
@@ -27,14 +49,14 @@ export const Block2 = () => {
         </Marquee>
         <Marquee speed={25} autoFill direction={"right"}>
           <div className="flex flex-row gap-8 h-[25vh] ms-8">
-            {[...Array(7)].map((_, index: any) => {
+            {arr2().map((val: number, index: any) => {
               return (
                 <Img
                   className={
                     "w-auto h-auto rounded-2xl select-none object-contain " +
                     hoverScale
                   }
-                  src={"../assets/block2/sqr0.webp"}
+                  src={`./block2/will${val}.webp`}
                   key={index}
                 />
               );
@@ -43,14 +65,14 @@ export const Block2 = () => {
         </Marquee>
         <Marquee speed={20} autoFill direction={"left"}>
           <div className="flex flex-row gap-8 h-[25vh] ms-8">
-            {[...Array(7)].map((_, index: any) => {
+            {arr3().map((val: number, index: any) => {
               return (
                 <Img
                   className={
                     "w-auto h-auto rounded-2xl select-none object-contain " +
                     hoverScale
                   }
-                  src={"../assets/block2/sqr0.webp"}
+                  src={`./block2/will${val}.webp`}
                   key={index}
                 />
               );
@@ -64,14 +86,14 @@ export const Block2 = () => {
           direction={"right"}
         >
           <div className="flex flex-row gap-8 h-[25vh] ms-8">
-            {[...Array(7)].map((_, index: any) => {
+            {arr4().map((val: number, index: any) => {
               return (
                 <Img
                   className={
                     "w-auto h-auto rounded-2xl select-none object-contain " +
                     hoverScale
                   }
-                  src={"../assets/block2/sqr0.webp"}
+                  src={`./block2/will${val}.webp`}
                   key={index}
                 />
               );
