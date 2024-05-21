45a61bd498e00a5c47b3da899e0b45694e40e500
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-20 21:16:49 -0400

    links

--- src/common/urls.ts
+++ src/common/urls.ts
@@ -2,9 +2,6 @@ export const CA = "Coming";
 export const TELEGRAM = "https://t.me/WillemDaChoice";
 export const TWITTER = "https://x.com/WillemDaChoice";
 
-export const DEXSCREENER =
-    " ";
-export const RAYDIUM =
-    " ";
-export const JUPITER =
-    " ";
+export const DEXSCREENER = "";
+export const RAYDIUM = "";
+export const JUPITER = "";
