5fb110c7b930d9d5451a622b64fae3542eb00580
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-23 00:29:50 -0400

    1

--- src/common/urls.ts
+++ src/common/urls.ts
@@ -1,12 +1,13 @@
 export const CA = "CeocaPX6WNeYKHvNKgKQC7wkMNNMViKgoTf2f87n1z7o";
+
 export const TELEGRAM = "https://t.me/WillemDaChoice";
+
 export const TWITTER = "https://x.com/WillemDaChoice";
 
-export const BUY_LINK =
-    `https://jupiter-terminal.dexscreener.com/?inputMint=${CA}&outputMint=So11111111111111111111111111111111111111112"`
+export const BUY_LINK = `https://jupiter-terminal.dexscreener.com/?inputMint=${CA}&outputMint=So11111111111111111111111111111111111111112"`;
 export const DEXSCREENER =
-    "https://dexscreener.com/solana/9nbnDgUAQ6SzS9T8JvUEGCzAJGGqcx8k72HVFB5rMHjc";
+  "https://dexscreener.com/solana/9nbnDgUAQ6SzS9T8JvUEGCzAJGGqcx8k72HVFB5rMHjc";
 export const RAYDIUM =
-    "https://raydium.io/swap/?inputMint=sol&outputMint=4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R";
+  "https://raydium.io/swap/?inputMint=sol&outputMint=4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R";
 export const JUPITER =
-    "https://jupiter-terminal.dexscreener.com/?inputMint=CeocaPX6WNeYKHvNKgKQC7wkMNNMViKgoTf2f87n1z7o&outputMint=So11111111111111111111111111111111111111112";
+  "https://jupiter-terminal.dexscreener.com/?inputMint=CeocaPX6WNeYKHvNKgKQC7wkMNNMViKgoTf2f87n1z7o&outputMint=So11111111111111111111111111111111111111112";
