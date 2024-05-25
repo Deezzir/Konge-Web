c3a62ff53c67b43e8299b6538142eb06f404130e
Iurii Kondrakov <55366304+Deezzir@users.noreply.github.com>
2024-05-25 00:40:39 -0400

    Loading

--- src/pages/landing/landing.tsx
+++ src/pages/landing/landing.tsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Marquee from "react-fast-marquee";
 import { Block1 } from "./block1";
 import { Block2 } from "./block2";
@@ -7,8 +8,34 @@ import { Block4 } from "./block4";
 import { Block5 } from "./block5";
 import DIVIDER from "../../assets/divider.webp";
 import { Img } from "react-image";
+import SWAY from "../../assets/block1/sway.webp";
 
 export const Landing = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+
+    return () => clearTimeout(timer); // Cleanup the timer on component unmount
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="top-0 right-[8%] -translate-y-[10px] absolute">
+          <Img
+            src={SWAY}
+            alt="sway"
+            className="h-[25vh] w-auto aspect-auto sway-monkey"
+          />
+        </div>
+        <div className="loader text-2xl">Loading...</div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center">
       <Block1 />
