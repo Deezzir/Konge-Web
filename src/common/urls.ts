ded207dd9f5aad80cf9da9c55e5947086dce1a0b
Viacheslav Verchonov <vverchonov@gmail.com>
2024-05-25 19:41:39 -0400

    1

--- src/common/urls.ts
+++ src/common/urls.ts
@@ -4,9 +4,9 @@ export const TELEGRAM = "https://t.me/kongekinge";
 
 export const TWITTER = "https://x.com/kongekinge";
 
-export const BUY_LINK = `buy`;
-export const DEXSCREENER = "dexscreener";
-export const RAYDIUM = "raydium";
-export const JUPITER = "jupiter";
+export const BUY_LINK = ``;
+export const DEXSCREENER = "";
+export const RAYDIUM = "";
+export const JUPITER = "";
 
 export const RPC_ENDPOINT = "https://api.mainnet-beta.solana.com";
